03bb276b8d0b48a50f737dbf3c1589fbf850c071
abdulrahman <abdooawad43@gmail.com>
2018-04-02 00:22:41 +0200

    implement postController

--- app/src/main/java/com/tga/Controller/PostController.java
+++ app/src/main/java/com/tga/Controller/PostController.java
@@ -1,5 +1,7 @@
 package com.tga.Controller;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.tga.models.CommentModel;
 import com.tga.models.PostModel;
 
@@ -21,8 +23,25 @@ public class PostController {
         postModel.date = date;
         postModel.userId = userId;
         postModel.commentsID = commentsID;
+        this.create();
     }
 
+    public void create()
+    {
+        FirebaseDatabase fd = FirebaseDatabase.getInstance();
+        DatabaseReference dRef = fd.getReference("posts");
+
+        postModel.id = dRef.push().getKey();
+        dRef.child(postModel.id).setValue(postModel);
+    }
+    public void update()
+    {
+        FirebaseDatabase fd = FirebaseDatabase.getInstance();
+        DatabaseReference dRef = fd.getReference("posts");
+        dRef.child(postModel.id).setValue(postModel);
+        dRef.child((postModel.id)).child("commentsID").setValue(postModel.commentsID);
+
+    }
     public String getId() {
         return postModel.id;
     }
@@ -36,7 +55,11 @@ public class PostController {
     }
 
     public void setContent(String content) {
+
         postModel.content = content;
+        FirebaseDatabase fd = FirebaseDatabase.getInstance();
+        DatabaseReference dRef = fd.getReference("posts");
+        dRef.child(getId()).child("content").setValue(postModel.content);
     }
 
     public String getDate() {
@@ -47,32 +70,61 @@ public class PostController {
         return postModel.userId;
     }
 
-    public ArrayList<String> getComments() {
+    public ArrayList<String> getComments()
+    {
+
         return postModel.commentsID;
     }
 
     public void addComment(String commentID) {
         postModel.commentsID.add(commentID);
+        FirebaseDatabase fd = FirebaseDatabase.getInstance();
+        DatabaseReference dRef=fd.getReference("posts");
+        dRef.child(getId()).child("commentsID").setValue(postModel.commentsID);
+
     }
 
-    public void delComment(String comntID){
+    public void delCommentId(String comntID){
+
         postModel.commentsID.remove(comntID);
+        FirebaseDatabase fd = FirebaseDatabase.getInstance();
+        DatabaseReference dRef = fd.getReference("posts");
+        dRef.child(getId()).child(getId()).setValue(postModel.commentsID);
     }
-
+    //
     public void editComment(String comntID){ }
 
-    public void delPost() { }
+    public void delPost(String postId) {
+        FirebaseDatabase fd = FirebaseDatabase.getInstance();
+        DatabaseReference dRef = fd.getReference("posts");
+        dRef.child(postId).removeValue();
+    }
 
-    public void editPost() { }
+    public void editPost() {
+        FirebaseDatabase fd = FirebaseDatabase.getInstance();
+        DatabaseReference dRef = fd.getReference("posts");
+        dRef.child(getId()).child("content").setValue(postModel.content);
+    }
     
     private class Comment {
         private CommentModel commentModel;
 
-        public Comment(String id, String content, String date, String userId){
+        public Comment( String id , String content, String date, String userId ){
             commentModel.id = id;
             commentModel.content = content;
             commentModel.date = date;
             commentModel.userId = userId;
+
+        }
+        public void createComment()
+        {
+            FirebaseDatabase fd = FirebaseDatabase.getInstance();
+            DatabaseReference dRef = fd.getReference("comments");
+            commentModel.id = dRef.push().getKey();
+            dRef.child(commentModel.id).setValue(commentModel);
+            //add comment Id to post comments Id
+            addComment(commentModel.id);
+
         }
 
         public String getId() {
@@ -95,8 +147,20 @@ public class PostController {
             return commentModel.userId;
         }
 
-        public void delComment(){ }
+        public void delComment(){
+            FirebaseDatabase fd = FirebaseDatabase.getInstance();
+            DatabaseReference dRef = fd.getReference("comments");
+            dRef.child(commentModel.id).removeValue();
+            //delete comment id from post comments id
+            delCommentId(commentModel.id);
+        }
 
-        public void editComment() { }
+        public void editComment( String newCommentContent) {
+            FirebaseDatabase fd = FirebaseDatabase.getInstance();
+            DatabaseReference dRef = fd.getReference("comments");
+            setContent(newCommentContent);
+            dRef.child(commentModel.id).setValue(commentModel);
+
+        }
     }
 }
