e2574ae95c3207f2ff2cb2192b163443ab7387c8
root <emad.waleed@yahoo.com>
2018-04-07 15:59:50 +0200

    Zezo add DB_Interface class

--- app/src/main/java/com/tga/Controller/DB_Interface.java
+++ app/src/main/java/com/tga/Controller/DB_Interface.java
@@ -10,5 +10,6 @@ public interface DB_Interface {
     public void saveToDB(); // save the model object of the controller to DB
     public void delFromDB(); // delete the object from DB
     public void updateToDB(); // update the model after changes
-    public ArrayList<Object> listAll();
+    // public static ArrayList<Object> listAll(); implement this function
+    // public static Object getByID(String id); implement this function it's hard to set it static here
 }
