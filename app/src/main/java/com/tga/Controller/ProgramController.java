9da41df5db3ac3c693aebe0e426a877c5b86c700
root <emad.waleed@yahoo.com>
2018-04-07 16:32:11 +0200

    Zezo update ProgramController class

--- app/src/main/java/com/tga/Controller/ProgramController.java
+++ app/src/main/java/com/tga/Controller/ProgramController.java
@@ -1,7 +1,13 @@
 package com.tga.Controller;
 
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.tga.models.DiscountModel;
 import com.tga.models.ProgramModel;
 
@@ -11,7 +17,12 @@ public class ProgramController implements DB_Interface{
 
     private ProgramModel programModel;
     private Discount discount;
-    private DatabaseReference dbRef;
+    private static DatabaseReference dbRef;
+
+    public ProgramController(ProgramModel pm){
+        this.programModel = pm;
+        this.setDiscount(pm.discountID);
+    }
 
     public ProgramController(String id, String title, ArrayList<String> placesID, String description,
                         String startDate, String endDate, String hotelName, String discountID,
@@ -25,6 +36,7 @@ public class ProgramController implements DB_Interface{
         programModel.endDate = endDate;
         programModel.hotelName = hotelName;
         programModel.discountID = discountID;
+        this.setDiscount(discountID);
         programModel.rate = rate;
         programModel.hitRate = hitRate;
         programModel.reviews = reviews;
@@ -115,18 +127,43 @@ public class ProgramController implements DB_Interface{
         saveToDB();
     }
 
-    @Override
-    public ArrayList<Object> listAll() {
-        return null;
+    public static ArrayList<ProgramController> listAll() {
+        final ArrayList<ProgramController> list = new ArrayList<>();
+        dbRef = FirebaseDatabase.getInstance().getReference("Programs");
+        dbRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot progSnapshot : dataSnapshot.getChildren()){
+                    ProgramModel model = progSnapshot.getValue(ProgramModel.class);
+                    if (model != null)
+                        list.add(new ProgramController(model));
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w("ProjectController", "Failed to read list values.", databaseError.toException());
+            }
+        });
+        return list;
     }
 
-    public void editProgram(String title, String description, String startDate,
-                            String endDate, String hotelName) {
-        this.setTitle(title);
-        this.setDescription(description);
-        this.setStartDate(startDate);
-        this.setEndDate(endDate);
-        this.setHotelName(hotelName);
+    public static ProgramController getByID(String id) {
+        dbRef = FirebaseDatabase.getInstance().getReference("Programs");
+        Query q = dbRef.equalTo(id);
+        final ProgramModel[] pm = new ProgramModel[1];
+        q.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                pm[0] = dataSnapshot.getValue(ProgramModel.class);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w("ProjectController", "Failed to read value by ID.", databaseError.toException());
+            }
+        });
+        return new ProgramController(pm[0]);
     }
 
     public void delFromDB() {
@@ -144,6 +181,15 @@ public class ProgramController implements DB_Interface{
         dbRef.child(this.programModel.id).setValue(null);
     }
 
+    public void editProgram(String title, String description, String startDate,
+                            String endDate, String hotelName) {
+        this.setTitle(title);
+        this.setDescription(description);
+        this.setStartDate(startDate);
+        this.setEndDate(endDate);
+        this.setHotelName(hotelName);
+    }
+
     public void rate(int n) {
         if (n > 5) { // for any error reason made rate > 5
             n %= 5;
@@ -157,6 +203,14 @@ public class ProgramController implements DB_Interface{
     public void setDiscount(String id, String endDate, double discountPercentage){
         discount = new Discount(id, endDate, discountPercentage);
         this.programModel.discountID = id;
+    }
+
+    public void setDiscount(String discountID){
+        if (discountID != "") {
+            this.discount = new Discount("", "", 0);
+            this.discount = this.discount.getByID(discountID);
+        } else
+            this.discount = null;
     }
 
     public String getDiscountID() {
@@ -176,6 +230,10 @@ public class ProgramController implements DB_Interface{
     private class Discount implements DB_Interface{
 
         public DiscountModel discountModel;
+
+        public Discount (DiscountModel dm){
+            this.discountModel = dm;
+        }
 
         public Discount(String id, String endDate, double discountPercentage){
             discountModel.id = id;
@@ -223,9 +281,22 @@ public class ProgramController implements DB_Interface{
             saveToDB();
         }
 
-        @Override
-        public ArrayList<Object> listAll() {
-            return null;
+        public Discount getByID(String id) {
+            dbRef = FirebaseDatabase.getInstance().getReference("Discounts");
+            Query q = dbRef.equalTo(id);
+            final DiscountModel[] dm = new DiscountModel[1];
+            q.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    dm[0] = dataSnapshot.getValue(DiscountModel.class);
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    Log.w("DiscountController", "Failed to read value by ID.", databaseError.toException());
+                }
+            });
+            return new Discount(dm[0]);
         }
 
         public void delFromDB() {
